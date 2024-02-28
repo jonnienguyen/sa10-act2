1e57de88b1238103436da4fe2e8eb24641bc6a4f
Jonnie Nguyen <jonnienguyen17@gmail.com>
2024-02-28 17:26:02 -0600

    Created a class for each name.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,20 +18,67 @@ class GildedRose
     when "Backstage passes to a TAFKAL80ETC concert"
       return backstage_tick
     end
+
   end
 
-  # Force normal tests to pass
   def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
+  end
+
+  def brie_tick
+    @item = Brie.new(quality, days_remaining)
+    item.tick
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+
+  def sulfuras_tick
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
+  end
+
+  def backstage_tick
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
+  end
+
+end
+
+
+class Normal
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
     @days_remaining -= 1
     return if @quality == 0
     @quality -= 1
     @quality -= 1 if @days_remaining <= 0
   end
 
-  # Force brie tests to pass
-  def brie_tick
-    @days_remaining -= 1
+end
 
+class Brie
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+    @days_remaining -= 1
     return if @quality >= 50
 
     @quality += 1
@@ -39,12 +86,29 @@ class GildedRose
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
 
-  # Force sulfuras tests to pass
-  def sulfuras_tick
+end
+
+
+class Sulfuras
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
   end
 
-  # Force backstage tests to pass
-  def backstage_tick
+  def tick
+  end
+
+end
+
+class Backstage
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
     @days_remaining -= 1
     return if @quality >= 50
     return @quality = 0 if @days_remaining < 0
@@ -53,4 +117,5 @@ class GildedRose
     @quality += 1 if @days_remaining < 10
     @quality += 1 if @days_remaining < 5
   end
+
 end
